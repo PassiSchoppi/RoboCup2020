685ed41369d074c547a05113136166467ef90386
PassiSchoppi <pressure875@tutanota.com>
2019-11-14 07:23:32 +0100

    start to code on alpha
    	new file:   alpha/alpha.ino
    	modified:   mega/mega.ino

--- alpha/alpha.ino
+++ alpha/alpha.ino
@@ -1,12 +1,9 @@
 #include "Arduino.h"
-#include "motor.h"
 
 void setup() {
   // put your setup code here, to run once:
-  motorInit();
 }
 
 void loop() {
-  // motorSetLeftSpeed(25);
-  // motorSetRightSpeed(25);
+	
 }
