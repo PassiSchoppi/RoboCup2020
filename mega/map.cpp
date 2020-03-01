91a3df5cc6c451e83f21c98bf31abb1a56194736
Pascal Schoppenhauer <PassiSchoppi@users.noreply.github.com>
2020-03-01 11:37:22 +0100

    start programing map

--- mega/map.cpp
+++ mega/map.cpp
@@ -1,18 +1,54 @@
 #include "map.h"
 
+Field map[100][100];
+int robot_is_at_x = 50;
+int robot_is_at_y = 50;
+uint8_t robot_is_facing = NORTH;
+
 void mapInit() 
 {
-	Field map[3][3];
+	
 }
 
-void mapNewField() 
+void mapNewField(int x, int y, int north, int east, int south, int west) 
 {
 	
 }
 
 void mapUpdateField() 
 {
+	uint8_t localSensorData[15];
+	uint8_t directionsInOrder[4];
+	sensorRead(&localSensorData[0]);
 	
+	map[robot_is_at_x, robot_is_at_y].visited = true;
+	map[robot_is_at_x, robot_is_at_y].seen = true;
+	
+	switch(robot_is_facing)
+	{
+		case 0:
+			directionsInOrder[4] = {1, 2, 3, 4};
+			break;
+		
+		case 1:
+			directionsInOrder[4] = {2, 3, 4, 1};
+			break;
+				
+		case 2:
+			directionsInOrder[4] = {3, 4, 1, 2};
+			break;
+				
+		case 3:
+			directionsInOrder[4] = {4, 1, 2, 3};
+			break;
+	}
+	
+	for(uint8_t i = 0; i < 4; ++i)
+	{
+		map[robot_is_at_x][robot_is_at_y].directions[i] = wallExists( directionsInOrder[i], &localSensorData[0]);
+		
+		if(map[robot_is_at_x][robot_is_at_y].directions[i] && 
+	}
 }
 
 void mapSearchForUnvisited() 
--- mega/map.h
+++ mega/map.h
@@ -3,14 +3,16 @@
 
 #include <Arduino.h>
 #include "config.h"
+#include "sensor.h"
 
 struct Field{
 	bool directions[4];
 	bool visited;
+	bool seen;
 };
 
 void mapInit();
-void mapNewField();
+void mapNewField(int x, int y, int north, int east, int south, int west);
 void mapUpdateField();
 void mapSearchForUnvisited();
 void mapWhereToDrive();
